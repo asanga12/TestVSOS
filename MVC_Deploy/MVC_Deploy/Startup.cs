f6135a75c14c445b146226af96f1fce8e93907e8
Asanga <asanga@msgroup.net>
2016-11-29 14:35:23 -0800

    first commit

---
+++ MVC_Deploy/MVC_Deploy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Deploy.Startup))]
+namespace MVC_Deploy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
